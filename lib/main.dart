a56f4b9da0dcd7e44d53e79872da30a5a70be9a1
yasser2020 <yezzat2020@gmail.com>
2022-08-12 14:57:40 +0200

    add Scroller

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          UserTransactions(),
+          SingleChildScrollView(child: UserTransactions()),
           
         ],
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,43 +10,48 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.purple)),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '\$${tx.amount}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple,
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Row(
               children: [
-                Text(
-                  tx.title,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.blueAccent),
-                ),
-                Text(DateFormat.yMMMMd().format(tx.date),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.purple)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${tx.amount}',
                     style: TextStyle(
-                      color: Colors.grey,
-                    ))
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.blueAccent),
+                    ),
+                    Text(DateFormat.yMMMMd().format(tx.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ))
+                  ],
+                )
               ],
-            )
-          ],
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
